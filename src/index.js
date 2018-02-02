d9abbff7f64488e6088b29cf94e61f1c7ff6f2ab
Nick Sonner <nicksonner@gmail.com>
2018-02-02 12:46:11 +1100

    add initial state

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import { createStore, combineReducers } from 'redux'
 
+const initialState = {
+	cart: [
+		{
+			product: 'bread 700g',
+			quantity: 2,
+			unitCost: 90
+		},
+		{
+			product: 'milk 500ml',
+			quantity: 1,
+			unitCost: 47
+		},
+	]
+}
+
 const productsReducer = function(state = [], action) {
 	return state
 }
 
-const store = createStore(reducer)
+const cartReducer = function(state = initialState, action) {
+	return state
+}
+
+const allReducers = {
+	products: productsReducer,
+	shoppingCart: cartReducer
+}
+
+const rootReducer = combineReducers(allReducers)
+
+let store = createStore(rootReducer)
+
+console.log("initialState: ", store.getState())
